f64455ace0205000dc7e36b006a228bcdcfbdd36
FiacreR <68591058+FiacreR@users.noreply.github.com>
2021-02-04 11:32:14 +1100

    Added dropdowns.

---
+++ Recombination/1why.js
@@ -0,0 +1,44 @@
+let leftSide, rightSide, topSide, bottomSide, middle, spring, k, a, electronMembrane, holeMembrane, metal, angle, rotationSpeed, pyramids, monofacial;
+let photons = [];
+let holes = [];
+let electrons = [];
+
+function setup() {
+  initialise();
+  reset();
+}
+
+function reset() {
+  let electron1 = new Electron(leftSide+10,middle+10, k, electrons);       
+  electrons.push(electron1);
+  let hole1 = new Hole(rightSide-10,middle+10, k, holes);       
+  holes.push(hole1);
+  let electron2 = new Electron(leftSide+10,middle-10, k, electrons);       
+  electrons.push(electron2);
+  let hole2 = new Hole(rightSide-10,middle-10, k, holes);       
+  holes.push(hole2);
+}
+
+function draw() {
+  if(frameCount % 60 === 0){
+    reset();
+  }
+  displayCellElements(1,0,0,0);
+  displayLegend(1,1,1,0,0,0,0);
+  electronHoleInteraction(0.008,2.5,0.07);
+  for (let i = 0; i < photons.length; i++) {
+    photons[i].move();
+    photons[i].display();
+  }
+  electrons.forEach(electron => {
+    electron.collide();
+    electron.move();
+    electron.display();
+  });
+  holes.forEach(hole => {
+    hole.collide();
+    hole.move();
+    hole.display();
+  });
+}
+
